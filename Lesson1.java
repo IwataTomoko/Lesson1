09a084c76be7bb803aa18ced9bb161f0d01f3bbe
IwataTomoko <iwatomo02468@gmail.com>
2022-05-17 19:14:44 +0900

    first commit

---
+++ Lesson1.java
@@ -0,0 +1,14 @@
+public class Lesson1 {
+    public static void main(String[] args) {
+
+        //10回連続でHello worldを出力、11回目からはHelloだけを出力.
+
+        for (int i = 0; i < 20; i++) {
+            if (i < 11) {
+                System.out.println("Hello Worid");
+            } else {
+                System.out.println("Hello");
+            }
+        }
+    }
+}
